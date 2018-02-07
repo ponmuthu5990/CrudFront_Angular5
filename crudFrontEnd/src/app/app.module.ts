7788f8bd37dd0132ef618f6947d9c3c6ebd0c090
ponmuthu5990 <30725846+ponmuthu5990@users.noreply.github.com>
2018-02-07 22:26:58 +0530

    Front End For Crud Operation

---
+++ crudFrontEnd/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ListuserComponent } from './components/listuser/listuser.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserService } from './shared-service/user.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListuserComponent,
+    UserFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
